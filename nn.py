52c223aec55a5278bd2ba169405650ccd9981f92
kyler-harrison <harri828@purdue.edu>
2023-03-15 14:21:06 -0400

    added comments and new function that computes loss w/o backprop

--- nn.py
+++ nn.py
@@ -19,17 +19,56 @@ class FeedFwdNN(nn.Module):
         self.loss = None
 
     def forward(self, input_data):
+        """
+        Description:
+        Forward pass of the neural network.
+
+        Inputs:
+        input_data: tensor of a single sample of data
+
+        Ouput:
+        returns tensor of network's final output
+        """
         x = self.dense1(input_data)
         x = self.relu1(x)
         x = self.out_dense(x)
         return x
 
     def backward(self, outputs, targets):
+        """
+        Description:
+        Backward pass of the neural network. Loss computed, optimizer
+        zeroed, loss and optimizer complete backward step.
+
+        Inputs:
+        outputs: tensor of network's predicted outputs of a given sample
+                 (i.e. output of forward pass)
+        targets: actual output tensor for given sample
+
+        Output:
+        returns loss value between outputs and targets
+        """
         self.loss = self.criteria(outputs, targets)
         loss_copy = self.loss
         self.optimizer.zero_grad()
         self.loss.backward()
         self.optimizer.step()
         return loss_copy
+
+    def compute_loss(self, outputs, targets):
+        """
+        Description:
+        Computes loss without actually backpropagating. Used for checking
+        network's progress with test data.
+
+        Inputs:
+        outputs: tensor of network's predicted outputs of a given sample
+                 (i.e. output of forward pass)
+        targets: actual output tensor for given sample
+
+        Output:
+        returns loss value between outputs and targets
+        """
+        return self.criteria(outputs, targets)
 
 
