d30105a733565cdf268dd078618e4b2e5656056a
kyler-harrison <harri828@purdue.edu>
2023-03-15 14:41:05 -0400

    made training much faster and loss calc much better by adding batching, also added a test dataset

--- train.py
+++ train.py
@@ -6,29 +6,45 @@ from nn import FeedFwdNN
 
 
 def main():
-    df = pd.read_csv("underdamped_training_data.csv")
+    # load and prepare train dataset
+    df = pd.read_csv("data/underdamped_independent_train.csv")
     feat_cols = [col for col in df.columns if col != "y"]
     X = torch.tensor(df[feat_cols].values).float()
     y = torch.tensor(df["y"].values).float()
     y = y.reshape(y.shape[0], 1)
 
-    input_len = X.shape[1]
-    net = FeedFwdNN(input_len, 1)
-    num_epochs = 10
-    mod_step = 100
-    pred = net.forward(X[0])
+    # load and prepare test dataset
+    test_df = pd.read_csv("data/underdamped_independent_test.csv")
+    X_test = torch.tensor(test_df[feat_cols].values).float()
+    y_test = torch.tensor(test_df["y"].values).float()
+    y_test = y_test.reshape(y_test.shape[0], 1)
 
+    # initialize network
+    input_len = X.shape[1]
+    num_outputs = 1  # nn predicts a scalar
+    model = FeedFwdNN(input_len, num_outputs)
+
+    # parameters of training loop
+    num_epochs = 10
+    batch_size = 10
+    batches = torch.arange(0, X.shape[0], batch_size)
+    
+    # training loop
     for epoch_idx in range(num_epochs):
         start = time.time()
         print(f"epoch: {epoch_idx}")
 
-        for i in range(X.shape[0]):
-            predicted_position = net.forward(X[i])
-            loss = net.backward(predicted_position, y[i])
+        for batch in batches:
+            X_batch = X[batch:batch + batch_size]
+            y_batch = y[batch:batch + batch_size]
+            y_predictions = model.forward(X_batch)
+            train_loss = model.backward(y_predictions, y_batch)
 
-        print(f"final loss = {loss}, time = {time.time() - start:.2f} seconds")
+        y_test_predictions = model.forward(X_test)
+        test_loss = model.compute_loss(y_test_predictions, y_test)
+        print(f"train loss = {train_loss}, test loss = {test_loss}, time = {time.time() - start:.2f} seconds")
 
-    torch.save(net.state_dict(), "underdamped_model.pt")
+    torch.save(model.state_dict(), "models/underdamped_good_1e6_t0=0_tf=50_ts=0.1.pt")
 
 
 if __name__ == "__main__":
